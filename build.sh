9266c88a3b55a0fe9f485c2f543f4d6fe753b442
Macauly Vivaldo <134652516+MacaulyV@users.noreply.github.com>
2024-11-23 22:15:41 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@
 apt-get update
 
 # 2. Instalar dependências do sistema necessárias
-apt-get install -y python3-distutils zlib1g-dev
+apt-get install -y python3-distutils zlib1g-dev build-essential gfortran libatlas-base-dev
 
 # 3. Atualizar o pip para garantir que estamos usando a versão mais recente
 pip install --upgrade pip
