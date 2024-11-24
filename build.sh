e3aafb5e193e49d62936de0ae867e8898a1c497f
Macauly Vivaldo <134652516+MacaulyV@users.noreply.github.com>
2024-11-23 22:05:53 -0300

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# 1. Atualizar os repositórios de pacotes do sistema
+apt-get update
+
+# 2. Instalar dependências do sistema necessárias
+apt-get install -y python3-distutils zlib1g-dev
+
+# 3. Atualizar o pip para garantir que estamos usando a versão mais recente
+pip install --upgrade pip
